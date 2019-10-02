ed5a05bc7b6900ed88f85789deeee7f629e357dc
AsianJohnBoi <jbssantias@hotmail.com>
2019-10-02 22:16:19 +1000

    new endpoint /analyse listens to POST requests

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const processTweets = require('../scripts/processTweets');
+const processAnalysis = require('../scripts/processAnalysis');
 
 var router = express.Router();
 
@@ -10,6 +12,21 @@ router.get('/test', function(req, res, next) {
 // *** Must Plug it after the API routes , otherwise the API will not works ***
 router.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+});
+
+router.post('/analyse', function(req, res, next) {
+
+  // store the search query
+  let query = req.body.query;
+
+  // Obtain all the tweets
+  processTweets.getTweets(query).then((response) => {
+    res.json(response);
+  })
+  .catch((err) => {
+    res.send("Error occured getting tweets");
+  });
+
 });
 
 module.exports = router;
