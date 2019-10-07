a18819abe26271398ecce994c925aa9512c1e11e
AsianJohnBoi <jbssantias@hotmail.com>
2019-10-07 22:28:24 +1000

    checks db

---
+++ server/services/searchDatabase.js
@@ -0,0 +1,15 @@
+var emotionModel = require('./storeEmotion');
+
+async function getEmotions(Datekey){
+    return new Promise((resolve, reject) => {
+        // let emotions = [];
+        emotionModel.find({date: Datekey}).then(response => {
+            // console.log(response);
+            resolve(JSON.parse(response));
+        }).catch(err => {
+            resolve(false);
+        })
+    })
+}
+
+module.exports = { getEmotions };
