4aa0cd28df8a0d47819c84e6822f095d3cc37d34
AsianJohnBoi <jbssantias@hotmail.com>
2019-10-16 12:40:16 +1000

    server streams data, error handling in place

--- server/routes/stream.js
+++ server/routes/stream.js
@@ -25,62 +25,76 @@ let myVar;
 let data = '';
 
 router.post('/stream', function(req, res, next) {
-    const query = req.body.query;
-    myVar = setInterval(myTimer, 10000);
+    try {
+        const query = req.body.query;
+        myVar = setInterval(myTimer, 10000);
 
-    async function myTimer() {
-        try {
-            // analyse the tweets
-            const sentiment = await analyseSentiment(data);
-            console.log(sentiment);
-    
-            // save to db
-            await new sentimentModel({ query, sentiment }).save();
-    
-            // flush data
-            data = '';
-        } catch (err) {
-            return err;
+        async function myTimer() {
+            try {
+                // analyse the tweets
+                const sentiment = await analyseSentiment(data);
+                console.log(sentiment);
+
+                // send to client
+                res.write(JSON.stringify(sentiment));
+                // console.log(res);
+        
+                // save to db
+                await new sentimentModel({ query, sentiment }).save();
+        
+                // flush data
+                data = '';
+            } catch (err) {
+                return err;
+            }
         }
-    }
 
-    var toSearch = {
-        track: [query],
-        language: 'en'
-    }
-    stream = T.stream('statuses/filter', toSearch );
-
-    console.log("Tracking: " + toSearch.track);
-    // turn on stream
-    stream.on('message', function(message) {
-        // console.log(message.created_at, message.text + '\n');
-        data += message.text + '\n';
-    })
-
-    //Check limit
-    stream.on('limit', function(message) {
-        console.log("Limit Reached: " + message);
-    });
-
-    stream.on('disconnect', function(message) {
-        console.log("Stream Disconnected: " + message);
-    });
-
-    stream.on('error', function(message) {
-        console.log("Stream error: " + message);
-    });
-
-    stream.on('reconnect', function (request, response, connectInterval) {
-        console.log("Attempting to reconnect");
-        if (response) {
-            console.log("Connection result: " + response);
+        var toSearch = {
+            track: [query],
+            language: 'en'
         }
-    });
+        stream = T.stream('statuses/filter', toSearch );
 
-    setTimeout(function() {
-        stream.stop(); // stops stream
-        console.log("Stream closed");
-    }, 900000);
+        console.log("Tracking: " + toSearch.track);
+        // turn on stream
+        stream.on('message', function(message) {
+            // console.log(message.created_at, message.text + '\n');
+            data += message.text + '\n';
+        })
+
+        //Check limit
+        stream.on('limit', function(message) {
+            console.log("Limit Reached: " + message);
+        });
+
+        stream.on('disconnect', function(message) {
+            console.log("Stream Disconnected: " + message);
+        });
+
+        stream.on('error', function(message) {
+            console.log("Stream error: " + message);
+        });
+
+        stream.on('reconnect', function (request, response, connectInterval) {
+            console.log("Attempting to reconnect");
+            if (response) {
+                console.log("Connection result: " + response);
+            }
+        });
+
+        setTimeout(function() {
+            clearInterval(myVar);
+            stream.stop(); // stops stream
+            console.log("Stream closed");
+            res.end();
+            // console.log(res.res);
+        }, 35000);
+    } catch (err) {
+        clearInterval(myVar);
+        console.log("stream error: ", err);
+        res.send("Stream error: ", err);
+    }
+    
 });
 
 router.post('/stop', function(req, res, next){
