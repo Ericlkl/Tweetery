6b8f9ea71d9aa437cd6cb5bf86a66bb518c3ee84
Eric Lee <Raidenlkl@gmail.com>
2019-10-27 22:26:50 +1000

    Remove test file

--- server/test.js
+++
@@ -1,8 +0,0 @@
-const TwitStream = require('./model/TwitStream');
-
-const twitStream = new TwitStream(['trump'], 'trump');
-
-setInterval(() => {
-  console.log('New Data');
-  console.log(twitStream.data);
-}, 5000);
