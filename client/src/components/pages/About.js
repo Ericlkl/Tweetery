c6a6f06bda3ed3e08d82241003d242cf4a1aa5df
AsianJohnBoi <jbssantias@hotmail.com>
2019-10-27 21:51:59 +1000

    changed title and added info about app

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -25,7 +25,20 @@ const About = () => {
             variant='body1'
             component='h3'
           >
-            Paragraph about this app
+            Tweetery aims to provide users with a visual representation of the analysis performed on searched tweets. 
+            Utilising the Twitter API for getting tweets into the webserver, cleaning the tweet body and performing 
+            emotional analysis with IBM Watson API. The results of the performed analysis are displayed on a graph for the user.
+            <br />
+            <br />
+            Tweetery is hosted on the Amazon Web Services (AWS) cloud provider. Utilising EC2 instances, an auto-scaling 
+            group, a launch configuration, and a load balancer. These services help tweetery perform consistently by 
+            regularly checking the health of our servers and scaling based on our scaling policy.
+            <br />
+            <br />
+            There are two features in this application: <br />
+            <strong>Static</strong> - Shows the emotional analytics of the searched topics.
+            <br />
+            <strong>Stream</strong> - Analyses the emotions of tweets in real time. (Can use only use one query and one user at a time!)
           </Typography>
 
           <Typography variant='body1' component='h3'>
